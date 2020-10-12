b0592352fa14f7d40cb7662559d95193b23beb57
TAKAYUKI MIYOSHI <pico.pico.hammer@icloud.com>
2020-10-12 15:00:57 +0900

    ・users、shops、favorites、times、area、osのそれぞれのテーブルを作成した。 ・1つのテーブルを作成するごとにロールバックで前の状態に戻せるかどうかを確認。

--- database/migrations/2020_10_12_051240_create_shops_table.php
+++ database/migrations/2020_10_12_051240_create_shops_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateShopsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,18 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('shops', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('img');
+            $table->string('tel');
+            $table->integer('open_time_id');
+            $table->integer('close_time_id');
+            $table->string('regular_holiday');
+            $table->string('location');
+            $table->string('access');
+            $table->integer('area_id');
+            $table->integer('os_id');
             $table->timestamps();
             $table->softDeletes();
         });
@@ -32,6 +37,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('shops');
     }
 }
--- database/migrations/2020_10_12_052906_create_favorites_table.php
+++ database/migrations/2020_10_12_052906_create_favorites_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateFavoritesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('favorites', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('user_id');
+            $table->integer('shop_id');
             $table->timestamps();
             $table->softDeletes();
         });
@@ -32,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('favorites');
     }
 }
